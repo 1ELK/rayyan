153116ad8be08e267ee9d0db2e695ba5b1908af0
1ELK <mabextr@gmail.com>
2025-02-17 05:01:18 +0300

    update

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function checkResolution() {
     var column3 = document.getElementById('column3');
     var avatar = document.getElementById('avatar');
-    if (window.innerWidth <= 768) { // Assuming mobile resolution is 768px or less
+
+    if (window.innerWidth <= 768) {
         column3.style.display = 'none';
-        avatar.style.display = 'none';
     } else {
         column3.style.display = 'block';
-        avatar.style.display = 'block';
     }
-    }
+}
 
-    window.addEventListener('resize', checkResolution);
-    window.addEventListener('load', checkResolution);
+// Check resolution on load and resize
+window.addEventListener('resize', checkResolution);
+window.addEventListener('load', checkResolution);
